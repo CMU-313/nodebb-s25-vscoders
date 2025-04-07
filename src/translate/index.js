f63e6befd421dcf3b0aefb8975d7cd5d8a7aa99a
Eddy Xu <91502960+Eddy114514@users.noreply.github.com>
2025-04-06 21:36:26 -0400

    Update index.js

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,9 +4,16 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	const TRANSLATOR_API = 'http://translator:5000/';
-	// const TRANSLATOR_API = 'http://127.0.0.1:5000';
-	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
-	const data = await response.json();
-	console.log(data);
-	return [data.is_english, data.translated_content];
+	try {
+		const response = await fetch(`${TRANSLATOR_API}/?content=${encodeURIComponent(postData.content)}`);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		const data = await response.json();
+		console.log(data);
+		return [data.is_english, data.translated_content];
+	} catch (error) {
+		console.error('Translation fetch error:', error);
+		return [true, postData.content];
+	}
 };
